5d4bd383aa531c48939c9a20f16fd67f3e3146eb
dininta <dinintaannisa@yahoo.com>
2015-11-19 14:28:12 +0700

    Update kNN: add full training and k-fold cross validation

--- src/weka/algorithm/kNN.java
+++ src/weka/algorithm/kNN.java
@@ -59,20 +59,8 @@ public class kNN {
                 if (temp.get(j).equals(attributes.get(j)))
                     counter++;
             }
-            //System.out.println("Jumlah atribut yang sama = " + counter);
             insertMin(counter, ds.getClass(i));
         }
-
-    /*    // print isi similiarity dan similiarity class
-        for (int i=0; i<k; i++) {
-            System.out.print(similiarity[i] + " ");
-        }
-        System.out.println();
-        for (int i=0; i<k; i++) {
-            System.out.print(similiarityclass[i] + " ");
-        }
-        System.out.println();
-    */
 
         // return modus dari similiarity class
         Map<String, Integer> classes = new HashMap<>();
@@ -82,7 +70,6 @@ public class kNN {
                 (!classes.containsKey(similiarityclass[i])) ? 1 : (int)classes.get(similiarityclass[i]) + 1
             );
         }
-
         Map.Entry<String, Integer> maxEntry = null;
         for (Map.Entry<String, Integer> entry : classes.entrySet()){
             if (maxEntry == null || entry.getValue() > (maxEntry.getValue()))
@@ -99,18 +86,54 @@ public class kNN {
 
     public static void main(String args[]){
         DataStore dataStore = new DataStore("weka/algorithm/car.data.txt");
-        kNN knn = new kNN(2,dataStore);
-        DataStore testData = dataStore;
-        int correct = 0;
-        
-        for (int i = 0; i < testData.getElementSize(); ++i) {
-            String prediction = knn.predict(testData.getAttributes(i));
-            System.out.println(prediction);
-            if (testData.getClass(i).equals(prediction))
-                ++correct;
-        }
+        System.out.println("kNN algorithm");
 
-        System.out.println("Result: " + correct + "/" + testData.getElementSize() + " correct");   
+        // full training
+        final int k = 3;
+        kNN knnFull = new kNN(k,dataStore);        
+        int correctFull = 0;
+        for (int i = 0; i < dataStore.getElementSize(); ++i) {
+            String prediction = knnFull.predict(dataStore.getAttributes(i));
+            if (dataStore.getClass(i).equals(prediction))
+                ++correctFull;
+        }
+        System.out.println("full training: " + correctFull + "/" + dataStore.getElementSize() + " correct");
+ 
+        // 10-fold cross-validation
+        final int x = 10;
+        final int partitionSize = dataStore.getElementSize() >= x ? dataStore.getElementSize()/x : 1;        
+        kNN knnFold = new kNN();
+        int maxCorrectFold = Integer.MIN_VALUE;
+        for (int fold = 0; fold < x; ++fold) {
+            // get partition for training and test
+            DataStore partitionTest = new DataStore(), partitionTraining = new DataStore();
+            int l = fold * partitionSize,
+                r = l + partitionSize - 1;
+            for (int i = 0; i < dataStore.getElementSize(); ++i)
+                if (i >= l && i <= r)
+                    partitionTest.add(dataStore.getAttributes(i), dataStore.getClass(i));
+                else
+                    partitionTraining.add(dataStore.getAttributes(i), dataStore.getClass(i));
+            kNN knnTraining = new kNN(k, partitionTraining);            
+            
+            // validation
+            int correct = 0;
+            for (int i = 0; i < partitionTest.getElementSize(); ++i) {
+                if (partitionTest.getClass(i).equals(knnTraining.predict(partitionTest.getAttributes(i))))
+                    ++correct;
+            }            
+            if (maxCorrectFold < correct) {
+                maxCorrectFold = correct;
+                knnFold = knnTraining;
+            }
+        }        
+        int correctFold = 0;
+        for (int i = 0; i < dataStore.getElementSize(); ++i) {
+            String prediction = knnFold.predict(dataStore.getAttributes(i));
+            if (dataStore.getClass(i).equals(prediction))
+                ++correctFold;
+        }
+        System.out.println(String.valueOf(x) + "-fold cross-validation: " + correctFold + "/" + dataStore.getElementSize() + " correct");
     }
 
 }
